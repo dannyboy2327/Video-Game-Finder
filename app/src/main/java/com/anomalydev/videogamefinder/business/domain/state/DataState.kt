b2b0a85bec06554549f2e2d671f17c61e4aeb20c
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-15 16:34:53 -0700

    Feat: added a data state class to pass events occurring for each use case

---
+++ app/src/main/java/com/anomalydev/videogamefinder/business/domain/state/DataState.kt
@@ -0,0 +1,33 @@
+package com.anomalydev.videogamefinder.business.domain.state
+
+data class DataState<out T>(
+    val data: T? = null,
+    val error: String? = null,
+    val loading: Boolean = false
+) {
+
+    companion object {
+
+        fun <T> success(
+            data: T,
+        ): DataState<T> {
+            return DataState(
+                data = data,
+            )
+        }
+
+        fun <T> error(
+            message: String,
+        ): DataState<T> {
+            return DataState(
+                error = message,
+            )
+        }
+
+        fun <T> loading(): DataState<T> {
+            return DataState(
+                loading = true,
+            )
+        }
+    }
+}
