1471157a9369ca37b49f310f8a3e0b1091cdf8d8
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-16 13:26:38 -0700

    Feat: added a database for games

---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/cache/database/GameDatabase.kt
@@ -0,0 +1,16 @@
+package com.anomalydev.videogamefinder.framework.datasource.cache.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.anomalydev.videogamefinder.framework.datasource.cache.model.GameEntity
+
+@Database(entities = [GameEntity::class], version = 1)
+abstract class GameDatabase: RoomDatabase() {
+
+    abstract fun gameDao(): GameDao
+
+    companion object {
+
+        const val DATABASE_NAME = "game_db"
+    }
+}
