d27165ae7e113ac2c4acf2d93dd4740a3235effb
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-16 18:48:59 -0700

    Feat: added a dto for game

---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/network/model/GameDto.kt
@@ -0,0 +1,28 @@
+package com.anomalydev.videogamefinder.framework.datasource.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class GameDto(
+
+    @SerializedName("id")
+    var id: Int,
+
+    @SerializedName("name")
+    var name: String,
+
+    @SerializedName("released")
+    var released: String,
+
+    @SerializedName("background_image")
+    var image: String,
+
+    @SerializedName("rating")
+    var rating: Int,
+
+    @SerializedName("rating_top")
+    var ratingTop: Int,
+
+    @SerializedName("updated")
+    var updated: String
+) {
+}
