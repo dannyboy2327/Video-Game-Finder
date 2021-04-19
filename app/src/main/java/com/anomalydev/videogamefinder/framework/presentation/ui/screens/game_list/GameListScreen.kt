764b39022d277f112b69411e0e06da8dabc4d7fd
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-19 16:06:17 -0700

    Fix: Removed unused imports

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_list/GameListScreen.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_list/GameListScreen.kt
@@ -1,26 +1,12 @@
 package com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_list
 
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.anomalydev.videogamefinder.R
-import com.anomalydev.videogamefinder.business.domain.model.Game
 import com.anomalydev.videogamefinder.framework.presentation.ui.components.GameList
 import com.anomalydev.videogamefinder.framework.presentation.ui.components.SearchBar
-import com.google.accompanist.coil.CoilImage
 
 
 @ExperimentalComposeUiApi
