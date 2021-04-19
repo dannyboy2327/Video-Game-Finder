df467180bf27fb0180df45f8a6506f4930eb5643
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-18 17:14:40 -0700

    Feat: added viewModel to my composable for GameListScreen

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/MainActivity.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/MainActivity.kt
@@ -3,12 +3,16 @@ package com.anomalydev.videogamefinder.framework.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.HiltViewModelFactory
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.anomalydev.videogamefinder.framework.presentation.ui.navigation.Screen
 import com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_details.GameDetailsScreen
 import com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_list.GameListScreen
+import com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_list.GameListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -25,8 +29,12 @@ class MainActivity : ComponentActivity() {
                 navController = navController,
                 startDestination = Screen.GameList.route,
             ) {
-                composable("gameList") {
-                    GameListScreen()
+                composable("gameList") { navBackStackEntry ->
+                    val factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry)
+                    val gameListViewModel: GameListViewModel = viewModel("GameListViewModel", factory)
+                    GameListScreen(
+                        viewModel = gameListViewModel,
+                    )
                 }
 
                 composable("gameDetails") {
