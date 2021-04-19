2a805ffc45a6f3fe064ec3bf824b953c4923114c
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-19 15:50:55 -0700

    Feat: added a scaffold to hold the search app bar

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_list/GameListScreen.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_list/GameListScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -17,9 +19,11 @@ import androidx.compose.ui.unit.dp
 import com.anomalydev.videogamefinder.R
 import com.anomalydev.videogamefinder.business.domain.model.Game
 import com.anomalydev.videogamefinder.framework.presentation.ui.components.GameList
+import com.anomalydev.videogamefinder.framework.presentation.ui.components.SearchBar
 import com.google.accompanist.coil.CoilImage
 
 
+@ExperimentalComposeUiApi
 @Composable
 fun GameListScreen(
     viewModel: GameListViewModel
@@ -29,8 +33,23 @@ fun GameListScreen(
 
     val loading = viewModel.loading.value
 
-    GameList(
-        games = games,
-        loading = loading
-    )
+    val query = viewModel.query.value
+
+    Scaffold(
+        topBar = {
+            SearchBar(
+                query = query,
+                onQueryChanged = viewModel::onQueryChanged,
+                onExecuteSearch = {
+                    viewModel.onTriggerEvent(GameListEvents.SearchGamesEvent)
+                },
+            )
+        }
+    ) {
+        GameList(
+            games = games,
+            loading = loading,
+            onScrollPositionChanged = viewModel::onChangeGameListPosition
+        )
+    }
 }
