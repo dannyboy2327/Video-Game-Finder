bff537ad3a83d3c0786ba5dbe8a56b96e4699490
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-18 19:28:13 -0700

    Feat: added an onTriggerEvent()
    This function will trigger the corresponding use case for the app.

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_list/GameListViewModel.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_list/GameListViewModel.kt
@@ -1,5 +1,6 @@
 package com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_list
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -8,30 +9,73 @@ import com.anomalydev.videogamefinder.business.domain.model.Game
 import com.anomalydev.videogamefinder.framework.datasource.network.abstraction.GameService
 import com.anomalydev.videogamefinder.framework.datasource.network.util.GameDtoMapper
 import com.anomalydev.videogamefinder.util.Constants
+import com.anomalydev.videogamefinder.util.Constants.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 import javax.inject.Named
 
 @HiltViewModel
 class GameListViewModel @Inject constructor(
-    gameService: GameService,
-    dtoMapper: GameDtoMapper,
-    @Named("auth_key") key: String,
-
+    private val gameService: GameService,
+    private val dtoMapper: GameDtoMapper,
+    @Named("auth_key") private val key: String,
 ): ViewModel() {
 
     val games: MutableState<List<Game>> = mutableStateOf(ArrayList())
 
+    val loading: MutableState<Boolean> = mutableStateOf(false)
+
     init {
+        onTriggerEvent(GameListEvents.SearchGamesEvent)
+    }
+
+
+    /**
+     *  This function will trigger an event for GameListScreen
+     */
+    private fun onTriggerEvent(event: GameListEvents) {
         viewModelScope.launch {
-            val result = gameService.searchGames(
-                key = key,
-                page = 1,
-                pageSize = Constants.PAGE_SIZE,
-                searchQuery = "Sonic"
-            )
-            games.value = dtoMapper.dtoToModelList(result.games)
+            try {
+                when (event) {
+                    is GameListEvents.SearchGamesEvent -> {
+                        newGameSearch()
+                    }
+
+                    is GameListEvents.SearchNextPageEvent -> {
+                        newSearchPage()
+                    }
+
+                    is GameListEvents.RestoreStateEvent -> {
+                        restoreState()
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "onTriggerEvent: $e, ${e.cause}")
+            }
         }
     }
+
+    // 1st Use Case
+    private suspend fun newGameSearch() {
+        val result = gameService.searchGames(
+            key = key,
+            page = 1,
+            pageSize = Constants.PAGE_SIZE,
+            searchQuery = "Sonic"
+        )
+        games.value = dtoMapper.dtoToModelList(result.games)
+    }
+
+    // 2nd Use Case
+    private fun newSearchPage() {
+
+    }
+
+    // 3rd Use Case
+    private fun restoreState() {
+
+    }
+
 }
