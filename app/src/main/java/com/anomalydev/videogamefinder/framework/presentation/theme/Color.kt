53350194f937f77734f21b1d33ffe4a68b3cb75c
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-21 21:52:55 -0700

    Feat: added colors for theme

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/theme/Color.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/theme/Color.kt
@@ -2,7 +2,37 @@ package com.anomalydev.videogamefinder.framework.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+// Light Theme Colors
+val red300 = Color(0xFFEF666C)
+val red400 = Color(0xFFFA3C46)
+val red500 = Color(0xFFFF1B27)
+val red600 = Color(0xFFF00028)
+val red700 = Color(0xFFDE0022)
+val red800 = Color(0xFFD1001A)
+
+// Secondary Colors
+val white50 = Color(0xFFFFFFFF)
+val white100 = Color(0xFFFAFAFA)
+val whiteGrey200 = Color(0xFFF5F5F5)
+val whiteGrey300 = Color(0xFFF0F0F0)
+val whiteGrey400 = Color(0xFFDEDEDE)
+val grey500 = Color(0xFFC2C2C2)
+val grey600 = Color(0xFF979797)
+val grey700 = Color(0xFF818181)
+val grey800 = Color(0xFF818181)
+
+// Dark Theme Colors
+val black400 = Color(0xFF9D9D9D)
+val black500 = Color(0xFF7B7B7B)
+val black600 = Color(0xFF555555)
+val black700 = Color(0xFF434343)
+val black800 = Color(0xFF262626)
+val black900 = Color(0xFF000000)
+
+
+val black1 = Color(0xFF222222)
+val black2 = Color(0xFF000000)
+
+val redErrorDark = Color(0xFFB00020)
+val redErrorLight = Color(0xFFEF5350)
