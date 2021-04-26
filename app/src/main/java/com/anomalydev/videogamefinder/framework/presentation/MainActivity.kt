d46809c6d69103d861e587f50fb3ee1e7e4b2423
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-25 21:20:39 -0700

    Feat: Added dataStore to hold state of theme

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/MainActivity.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.anomalydev.videogamefinder.framework.presentation
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.hilt.navigation.HiltViewModelFactory
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
@@ -15,13 +22,27 @@ import com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_lis
 import com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_list.GameListViewModel
 import com.anomalydev.videogamefinder.framework.presentation.ui.screens.settings.Settings
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+    private val isDark = mutableStateOf(true)
+
     @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Checks dataStore for darkTheme boolean
+        observeDataStore()
+
         setContent {
             // Used to remember nav state in the graph
             val navController = rememberNavController()
@@ -35,6 +56,7 @@ class MainActivity : ComponentActivity() {
                     val factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry)
                     val gameListViewModel: GameListViewModel = viewModel("GameListViewModel", factory)
                     GameListScreen(
+                        isDarkTheme = isDark.value,
                         viewModel = gameListViewModel,
                         onNavigateToGameDetailScreen = navController::navigate,
                         onNavigateToSettingsScreen = navController::navigate,
@@ -46,14 +68,45 @@ class MainActivity : ComponentActivity() {
                     arguments = listOf(navArgument("gameId") { type = NavType.IntType})
                 ) { navBackStackEntry ->
                     GameDetailsScreen(
+                        isDarkTheme = isDark.value,
                         gameId = navBackStackEntry.arguments?.getInt("gameId"),
                     )
                 }
 
                 composable("settings") { navBackStackEntry ->
-                    Settings()
+                    Settings(
+                        isDarkTheme = isDark.value,
+                        onToggleTheme = { onToggleTheme() }
+                    )
                 }
             }
         }
+    }
+
+    /**
+     *  Responsible for changing dark theme preference value on trigger
+     */
+    private fun onToggleTheme() {
+        CoroutineScope(Dispatchers.Main).launch{
+            dataStore.edit { preferences ->
+                val current = preferences[DARK_THEME_KEY]?: false
+                preferences[DARK_THEME_KEY] = !current
+            }
+        }
+    }
+
+    /**
+     *  Responsible for observing any changes when darkTheme value changes
+     */
+    private fun observeDataStore() {
+        dataStore.data.onEach { preferences ->
+            preferences[DARK_THEME_KEY]?.let { isDarkTheme ->
+                isDark.value = isDarkTheme
+            }
+        }.launchIn(CoroutineScope(Dispatchers.Main))
+    }
+
+    companion object {
+        private val DARK_THEME_KEY = booleanPreferencesKey("dark_theme_key")
     }
 }
