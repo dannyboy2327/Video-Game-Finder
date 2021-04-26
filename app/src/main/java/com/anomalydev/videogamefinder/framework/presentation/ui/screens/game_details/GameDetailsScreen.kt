a99ae87dacd70af379b91951b1c91dcb7b7c40a7
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-25 21:25:17 -0700

    Feat: Added param for isDarkTheme boolean

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_details/GameDetailsScreen.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_details/GameDetailsScreen.kt
@@ -1,9 +1,11 @@
 package com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_details
 
 import androidx.compose.runtime.Composable
+import com.anomalydev.videogamefinder.framework.presentation.theme.VideoGameFinderTheme
 
 @Composable
 fun GameDetailsScreen(
+    isDarkTheme: Boolean,
     gameId: Int?,
 ) {
 
