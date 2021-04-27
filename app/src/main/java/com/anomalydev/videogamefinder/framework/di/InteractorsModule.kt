2f99ebf4f564faf8e1e0f55e8e23e755e31af3e7
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-04-26 21:30:19 -0700

    Feat: Added a module for SearchGames for view model

---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/di/InteractorsModule.kt
@@ -0,0 +1,35 @@
+package com.anomalydev.videogamefinder.framework.di
+
+import com.anomalydev.videogamefinder.business.interactors.game_list.SearchGames
+import com.anomalydev.videogamefinder.framework.datasource.cache.database.GameDao
+import com.anomalydev.videogamefinder.framework.datasource.cache.util.GameEntityMapper
+import com.anomalydev.videogamefinder.framework.datasource.network.abstraction.GameService
+import com.anomalydev.videogamefinder.framework.datasource.network.util.GameDtoMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object InteractorsModule {
+
+    @ViewModelScoped
+    @Provides
+    fun provideSearchGames(
+        gameService: GameService,
+        gameDao: GameDao,
+        gameEntityMapper: GameEntityMapper,
+        gameDtoMapper: GameDtoMapper,
+    ): SearchGames {
+        return SearchGames(
+            gameDao = gameDao,
+            gameService = gameService,
+            entityMapper = gameEntityMapper,
+            dtoMapper = gameDtoMapper,
+        )
+    }
+}
