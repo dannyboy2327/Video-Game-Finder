055aa80f21f0f8b6bf174c3a704bc25b78d498cd
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-05-02 17:50:24 -0700

    Fix: Updated database version

--- app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/cache/database/GameDatabase.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/cache/database/GameDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.anomalydev.videogamefinder.framework.datasource.cache.model.GameEntity
 
-@Database(entities = [GameEntity::class], version = 2)
+@Database(entities = [GameEntity::class], version = 1)
 abstract class GameDatabase: RoomDatabase() {
 
     abstract fun gameDao(): GameDao
