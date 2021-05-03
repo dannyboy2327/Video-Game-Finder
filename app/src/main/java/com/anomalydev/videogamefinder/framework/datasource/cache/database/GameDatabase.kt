b379f5aa77d23e5ca41818e71e1e701baeb8a2c0
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-05-03 15:30:35 -0700

    Feat: Added type converter to database

--- app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/cache/database/GameDatabase.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/cache/database/GameDatabase.kt
@@ -2,9 +2,12 @@ package com.anomalydev.videogamefinder.framework.datasource.cache.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.anomalydev.videogamefinder.framework.datasource.cache.model.GameEntity
+import com.anomalydev.videogamefinder.framework.datasource.cache.util.Converter
 
 @Database(entities = [GameEntity::class], version = 1)
+@TypeConverters(Converter::class)
 abstract class GameDatabase: RoomDatabase() {
 
     abstract fun gameDao(): GameDao
