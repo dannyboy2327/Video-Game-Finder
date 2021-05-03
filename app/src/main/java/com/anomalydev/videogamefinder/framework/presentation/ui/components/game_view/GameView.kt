f7f46b14c460e98b4228606244ab0d9ba5a29218
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-05-02 17:49:23 -0700

    Feat: Added composable for GameView

---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/components/game_view/GameView.kt
@@ -0,0 +1,81 @@
+package com.anomalydev.videogamefinder.framework.presentation.ui.components
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.anomalydev.videogamefinder.business.domain.model.Game
+import com.anomalydev.videogamefinder.framework.presentation.ui.components.game_view.GameViewWebsite
+
+@Composable
+fun GameView(
+    game: Game,
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(MaterialTheme.colors.background)
+    ) {
+        GameImageHeading(
+            game = game,
+        )
+
+        GameImageBody(
+            game = game,
+        )
+
+        Divider(
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                ),
+            thickness = 1.dp,
+            color = MaterialTheme.colors.onBackground,
+        )
+
+        Text(
+            text = "Website",
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(
+                    top = 8.dp,
+                    start = 16.dp,
+                ),
+            style = MaterialTheme.typography.h5,
+        )
+
+        GameViewWebsite(
+            website = game.website,
+        )
+
+        Divider(
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                ),
+            thickness = 1.dp,
+            color = MaterialTheme.colors.onBackground,
+        )
+
+        Text(
+            text = "Reviews",
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(
+                    top = 8.dp,
+                    start = 16.dp,
+                ),
+            style = MaterialTheme.typography.h5,
+        )
+    }
+}
---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/components/game_view/GameViewHeading.kt
@@ -0,0 +1,133 @@
+package com.anomalydev.videogamefinder.framework.presentation.ui.components
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.anomalydev.videogamefinder.R
+import com.anomalydev.videogamefinder.business.domain.model.Game
+import com.anomalydev.videogamefinder.util.Constants
+import com.google.accompanist.coil.CoilImage
+import com.gowtham.ratingbar.RatingBar
+import com.gowtham.ratingbar.RatingBarStyle
+
+@Composable
+fun GameImageHeading(
+    game: Game
+) {
+
+    var ratingValue: Float = rememberSaveable { game.rating }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        CoilImage(
+            data = game.background_image,
+            contentDescription = game.name,
+            fadeIn = true,
+            error = {
+                Image(
+                    painter = painterResource(id = R.drawable.empty_plate),
+                    contentDescription = "Image not viewable"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(260.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomStart)
+                .padding(
+                    start = 8.dp,
+                    bottom = 8.dp,
+                    end = 6.dp,
+                ),
+        ) {
+            Text(
+                text = game.name,
+                color = MaterialTheme.colors.onPrimary,
+                style = MaterialTheme.typography.h3,
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                RatingBar(
+                    value = ratingValue,
+                    size = 12.dp,
+                    onRatingChanged = {
+                        ratingValue = it
+                        Log.d(Constants.TAG, "GameView: $it")
+                    },
+                    activeColor = Color.Red,
+                    inactiveColor = Color.White,
+                    ratingBarStyle = RatingBarStyle.Normal,
+                )
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_circle),
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.onBackground,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(
+                            start = 6.dp
+                        )
+                )
+
+                Text(
+                    text = "${game.playtime}hrs",
+                    color = MaterialTheme.colors.onBackground,
+                    style = MaterialTheme.typography.subtitle2,
+                    fontSize = 12.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(
+                            start = 6.dp
+                        )
+                )
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_circle),
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.onBackground,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(
+                            start = 6.dp
+                        )
+                )
+
+                Text(
+                    text = game.released,
+                    color = MaterialTheme.colors.onBackground,
+                    style = MaterialTheme.typography.subtitle2,
+                    fontSize = 12.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(
+                            start = 6.dp
+                        )
+                )
+            }
+
+        }
+    }
+}
