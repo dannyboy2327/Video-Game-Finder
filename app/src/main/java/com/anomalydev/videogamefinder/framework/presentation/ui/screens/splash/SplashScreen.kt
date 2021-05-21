01e8a2c322b974eee6b4976956a25c4e07753972
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-05-20 18:45:19 -0700

    Fix: Added a mssing import

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/splash/SplashScreen.kt
@@ -1,16 +1,8 @@
 package com.anomalydev.videogamefinder.framework.presentation.ui.screens.splash
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.anomalydev.videogamefinder.framework.presentation.theme.VideoGameFinderTheme
