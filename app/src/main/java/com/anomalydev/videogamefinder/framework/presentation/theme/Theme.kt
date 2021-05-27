30494cfa26a4fe558bb55f0269860ba07f38894f
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-05-26 23:28:25 -0700

    Feat: Added a list of queues to the theme

--- app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/theme/Theme.kt
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/theme/Theme.kt
@@ -1,12 +1,24 @@
 package com.anomalydev.videogamefinder.framework.presentation.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.anomalydev.videogamefinder.framework.presentation.ui.components.dialog.GenericDialog
+import com.anomalydev.videogamefinder.framework.presentation.ui.components.dialog.GenericDialogInfo
+import com.anomalydev.videogamefinder.framework.presentation.ui.components.dialog.NegativeAction
+import com.anomalydev.videogamefinder.framework.presentation.ui.components.dialog.PositiveAction
+import com.anomalydev.videogamefinder.framework.presentation.ui.util.DialogQueue
+import java.util.*
 
 private val DarkColorPalette = darkColors(
     primary = Black900,
@@ -41,6 +53,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun VideoGameFinderTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dialogQueue: Queue<GenericDialogInfo>? = null,
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
@@ -53,6 +66,30 @@ fun VideoGameFinderTheme(
         colors = colors,
         typography = CodaTypography,
         shapes = Shapes,
-        content = content
-    )
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(color = if (!darkTheme) Grey700 else Grey900)
+        ) {
+            content()
+            dialogQueue?.let {
+                ProcessDialogQueue(dialogQueue = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ProcessDialogQueue(
+    dialogQueue: Queue<GenericDialogInfo>
+) {
+    dialogQueue.peek()?.let { dialogInfo ->
+        GenericDialog(
+            onDismiss = dialogInfo.onDismiss,
+            title = dialogInfo.title,
+            description = dialogInfo.description,
+            positiveAction = dialogInfo.positiveAction,
+            negativeAction = dialogInfo.negativeAction,
+        )
+    }
 }
