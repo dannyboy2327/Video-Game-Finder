95a3a72c5f05ac7f73d7faa7d59c30941cdc1901
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-06-28 22:47:12 -0700

    Feat: Added a constant for a game id key for viewModel

---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/presentation/ui/screens/game_trailers/util/GameTrailersViewModelConstants.kt
@@ -0,0 +1,6 @@
+package com.anomalydev.videogamefinder.framework.presentation.ui.screens.game_trailers.util
+
+object GameTrailersViewModelConstants {
+
+    const val GAME_ID_KEY = "game_id_key"
+}
