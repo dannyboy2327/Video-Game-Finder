998adcfaeda6443f6e20467131bce9947cf0361e
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-06-28 22:53:29 -0700

    Feat: Added a response for GameTrailers from the network

---
+++ app/src/main/java/com/anomalydev/videogamefinder/framework/datasource/network/response/GameTrailersResponse.kt
@@ -0,0 +1,19 @@
+package com.anomalydev.videogamefinder.framework.datasource.network.response
+
+import com.anomalydev.videogamefinder.framework.datasource.network.model.GameTrailersDto
+import com.google.gson.annotations.SerializedName
+
+data class GameTrailersResponse(
+
+    @SerializedName("count")
+    var count: Int,
+
+    @SerializedName("next")
+    var next: String,
+
+    @SerializedName("previous")
+    var previous: String,
+
+    @SerializedName("results")
+    var gameTrailers: List<GameTrailersDto>
+)
